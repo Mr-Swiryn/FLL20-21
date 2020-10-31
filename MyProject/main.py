20d120179b2bcd1b0bd17771ba8b2978255c8c7b
Mr-Swiryn <73761402+Mr-Swiryn@users.noreply.github.com>
2020-10-31 16:34:17 -0600

    uploaded via web

---
+++ MyProject/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env pybricks-micropython
+
+import other
+from pybricks.tools import wait
+
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+other.doBeep()
